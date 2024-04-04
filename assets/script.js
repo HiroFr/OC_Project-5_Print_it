876947956d816e58e4d53ab5150ab01d88a13343
HiroFr <ichiro.vf@gmail.com>
2024-04-04 11:34:24 +0200

    update : EventListener/dots and add/remove style css

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,56 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+var currentSlideIndex = 0;
+
+var arrowLeft = document.querySelector(".arrow_left");
+var arrowRight = document.querySelector(".arrow_right");
+
+var nbrSlides = slides.length;
+console.log(nbrSlides);
+
+var divDots = document.querySelector("#banner > div");
+
+/* ---- */
+/* Pour ajouter le nbr de dot en fonction de la taille du tableau */
+
+for (var i = 0; i < nbrSlides; i++) {
+	var dot = document.createElement("span");
+	dot.classList.add("dot");
+	divDots.appendChild(dot);
+}
+
+/* ---- */
+/* Pour mettre à jour le contenu du carrousel */
+var bannerImg = banner.querySelector(".banner-img");
+var bannerText = banner.querySelector("p");
+
+function updateCarousel() {
+	bannerImg.src = `./assets/images/slideshow/${slides[currentSlideIndex].image}`;
+	bannerText.innerHTML = slides[currentSlideIndex].tagLine;
+}
+
+/* ---- */
+/* EventListener pour la flèche de gauche */
+
+arrowLeft.addEventListener("click", function () {
+	console.log("Je clique sur la flèche de gauche !")
+
+	currentSlideIndex--;
+	updateCarousel();
+
+});
+
+/* ---- */
+/* EventListener pour la flèche de droite */
+
+arrowRight.addEventListener("click", function () {
+	console.log("Je clique sur la flèche de droite !")
+
+	currentSlideIndex++;
+    updateCarousel();
+});
+
+// Mettre à jour le contenu initial du carrousel
+updateCarousel();
